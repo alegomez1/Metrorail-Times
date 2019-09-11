1ca5ee941da53b4ab8ce9fe7e1efd4f0fa61c013
alejandrogomez <alegomez1@me.com>
2019-09-10 22:11:10 -0400

    API works for Dadeland North trains

--- Metrorail/public/Javascript/index.js
+++ Metrorail/public/Javascript/index.js
@@ -1,1 +1,27 @@
-console.log('script connected')
+const proxy = 'https://cors-anywhere.herokuapp.com/'
+const api = `http://miami-transit-api.herokuapp.com/api/TrainTracker.json`
+
+fetch(api).then(response =>{
+    return response.json();
+}).then(data => {
+console.log(data)
+//Dadeland Info
+const dadeland = data.RecordSet.Record[6]
+const dadelandFirstNorthBoundTrainTime = dadeland.NB_Time1
+const dadelandFirstNorthBoundTrainArrival = dadeland.NB_Time1_Arrival
+const dadelandFSecondtNorthBoundTrainTime = dadeland.NB_Time2
+const dadelandSecondNorthBoundTrainArrival = dadeland.NB_Time2_Arrival
+
+document.querySelector("body > div:nth-child(3) > h5:nth-child(2)").innerText += ` ${dadelandFirstNorthBoundTrainTime}.....ETA: ${dadelandFirstNorthBoundTrainArrival}`
+document.querySelector("body > div:nth-child(3) > h5:nth-child(3)").innerText += ` ${dadelandFSecondtNorthBoundTrainTime}.....ETA: ${dadelandSecondNorthBoundTrainArrival}`
+
+
+//Brickell Info
+
+
+
+
+})
+
+
+console.log("Script ran")
