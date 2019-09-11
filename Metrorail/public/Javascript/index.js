625239aeedbd30a0fcfbbe47692d14f95b641918
alejandrogomez <alegomez1@me.com>
2019-09-11 08:58:09 -0400

    Finished API for both stations

--- Metrorail/public/Javascript/index.js
+++ Metrorail/public/Javascript/index.js
@@ -7,13 +7,23 @@ fetch(api).then(response =>{
 console.log(data)
 //Dadeland Info
 const dadeland = data.RecordSet.Record[6]
+//North Trains
 const dadelandFirstNorthBoundTrainTime = dadeland.NB_Time1
 const dadelandFirstNorthBoundTrainArrival = dadeland.NB_Time1_Arrival
-const dadelandSecondtNorthBoundTrainTime = dadeland.NB_Time2
+const dadelandSecondNorthBoundTrainTime = dadeland.NB_Time2
 const dadelandSecondNorthBoundTrainArrival = dadeland.NB_Time2_Arrival
 
 document.querySelector("body > div:nth-child(3) > h5:nth-child(2)").innerText += ` ${dadelandFirstNorthBoundTrainTime}.....ETA: ${dadelandFirstNorthBoundTrainArrival}`
-document.querySelector("body > div:nth-child(3) > h5:nth-child(3)").innerText += ` ${dadelandSecondtNorthBoundTrainTime}.....ETA: ${dadelandSecondNorthBoundTrainArrival}`
+document.querySelector("body > div:nth-child(3) > h5:nth-child(3)").innerText += ` ${dadelandSecondNorthBoundTrainTime}.....ETA: ${dadelandSecondNorthBoundTrainArrival}`
+
+//South Trains
+const dadelandFirstSouthBoundTrainTime = dadeland.SB_Time1
+const dadelandFirstSouthBoundTrainArrival = dadeland.SB_Time1_Arrival
+const dadelandSecondSouthBoundTrainTime = dadeland.SB_Time2
+const dadelandSecondSouthBoundTrainArrival = dadeland.SB_Time2_Arrival
+
+document.querySelector("body > div:nth-child(4) > h5:nth-child(2)").innerText += ` ${dadelandFirstSouthBoundTrainTime}.....ETA: ${dadelandFirstSouthBoundTrainArrival}`
+document.querySelector("body > div:nth-child(4) > h5:nth-child(3)").innerText += ` ${dadelandSecondSouthBoundTrainTime}.....ETA: ${dadelandSecondSouthBoundTrainArrival}`
 
 
 //Brickell Info
@@ -21,16 +31,21 @@ const brickell = data.RecordSet.Record[1]
 //North Trains
 const brickellFirstNorthBoundTrainTime = brickell.NB_Time1
 const brickellFirstNorthBoundTrainArrival = brickell.NB_Time1_Arrival
-const brickellSecondtNorthBoundTrainTime = brickell.NB_Time2
+const brickellSecondNorthBoundTrainTime = brickell.NB_Time2
 const brickellSecondNorthBoundTrainArrival = brickell.NB_Time2_Arrival
 
 document.querySelector("body > div:nth-child(6) > h5:nth-child(2)").innerText += ` ${brickellFirstNorthBoundTrainTime}.....ETA: ${brickellFirstNorthBoundTrainArrival}`
-document.querySelector("body > div:nth-child(6) > h5:nth-child(3)").innerText += ` ${brickellSecondtNorthBoundTrainTime}.....ETA: ${brickellSecondNorthBoundTrainArrival}`
+document.querySelector("body > div:nth-child(6) > h5:nth-child(3)").innerText += ` ${brickellSecondNorthBoundTrainTime}.....ETA: ${brickellSecondNorthBoundTrainArrival}`
+
 //South Trains
 const brickellFirstSouthBoundTrainTime = brickell.SB_Time1
 const brickellFirstSouthBoundTrainArrival = brickell.SB_Time1_Arrival
-const brickellSecondtSouthBoundTrainTime = brickell.SB_Time2
+const brickellSecondSouthBoundTrainTime = brickell.SB_Time2
 const brickellSecondSouthBoundTrainArrival = brickell.SB_Time2_Arrival
+
+document.querySelector("body > div:nth-child(7) > h5:nth-child(2)").innerText += ` ${brickellFirstSouthBoundTrainTime}.....ETA: ${brickellFirstSouthBoundTrainArrival}`
+document.querySelector("body > div:nth-child(7) > h5:nth-child(3)").innerText += ` ${brickellSecondSouthBoundTrainTime}.....ETA: ${brickellSecondSouthBoundTrainArrival}`
+
 
 
 
